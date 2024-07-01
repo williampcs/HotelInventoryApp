14816e084d0287966580fea06e828d2bcc76d032
william <williampcs0203@gmail.com>
2024-07-01 12:20:24 +0800

    create new component rooms-list under rooms

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss']
+})
+export class RoomsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
